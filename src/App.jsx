2f0ab0463bd8f5991c284bbba78ccf239586b66e
Guanyu Lin <limkoaniun@gmail.com>
2025-11-16 15:49:35 +1100

    implement LifecycleLogger with class component and integrate into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import LifecycleLogger from './components/LifecycleLogger';
 
 function App() {
-  const [showLogger, setShowLogger] = useState(false);
+    const [showLogger, setShowLogger] = useState(false);
 
-  return (
-    <div className='container'>
-      <h2>React Lifecycle Playground</h2>
+    return (
+        <div className='container'>
+            <h2>React Lifecycle Playground</h2>
 
-      {/* Toggle LifecycleLogger */}
-      <button
-        className='primary-btn'
-        onClick={() => setShowLogger(!showLogger)}
-      >
-        {showLogger ? 'Unmount Logger' : 'Mount Logger'}
-      </button>
+            {/* Toggle LifecycleLogger */}
+            <button
+                className='primary-btn'
+                onClick={() => setShowLogger(!showLogger)}
+            >
+                {showLogger ? 'Unmount Logger' : 'Mount Logger'}
+            </button>
 
-      {showLogger && <LifecycleLogger />}
-    </div>
-  );
+            {/*{showLogger && <LifecycleLogger message="Hello from LifecycleLogger!"/>}*/}
+            {showLogger && <LifecycleLogger/>}
+        </div>
+    );
 }
+
 export default App;
---
+++ src/components/LifecycleLogger.jsx
@@ -0,0 +1,47 @@
+import {Component} from 'react';
+
+class LifecycleLogger extends Component {
+    constructor(props) {
+        super(props); // we need to call super() in order to use the props in the constructor
+        // and this super() is from the Component class we are inheriting from
+        console.log("Component initialized!");
+        this.state = {
+            count: 0
+        } // this equivalent to const [count, setCount] = useState(0);
+    }
+
+    componentDidMount() {
+        // we do API calls in this lifecycle method AKA step 1 Mounting
+        console.log("Component mounted!");
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.count !== this.state.count) {
+            console.log("Component updated!", this.state.count);
+        }
+    }
+
+    componentWillUnmount() {
+        // we do cleanup in this lifecycle method AKA step 4 Unmounting
+        console.log("Component unmounted!");
+    }
+
+    incrementCount = () => {
+        this.setState(prevState => ({count: prevState.count + 1}));
+    }
+
+    render() {
+        return (
+            <div className="logger-container">
+                <h2>LifecycleLogger (Class Component)</h2>
+                <p>{this.props.message}</p>
+                <p>Count: {this.state.count}</p>
+                <button onClick={this.incrementCount} className="secondary-btn">
+                    Increment
+                </button>
+            </div>
+        )
+    }
+}
+
+export default LifecycleLogger;
